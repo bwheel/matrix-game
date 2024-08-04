bc1561ce6481bf2990907084b316b784898f6029
Byron Wheeler <bwheel1990@gmail.com>
2024-08-03 21:40:06 -0500

    reset to python

--- cpp/src/Components/Color.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cstdint>
-
-namespace Components {
-struct Color {
-  std::uint8_t r, g, b, a;
-};
-} // namespace Components
--- cpp/src/Systems/RotationSystem.cpp
+++
@@ -1,9 +0,0 @@
-#include <entt/entt.hpp>
-
-#include <RotationSystem.hpp>
-
-namespace Systems {
-void RotationSystem::Process(entt::registry &registry) {
-  // TODO: implement
-}
-} // namespace Systems
